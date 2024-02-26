e8de146f7f14c695ba10940246cd46039342b6c5
AfonsoRM <124623524+AfonsoRM@users.noreply.github.com>
2024-02-26 20:47:42 +0000

    cooked up api for getting user interests and updating sclae, sql change to input a scale value of 0 for every interest

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -25,6 +25,42 @@ def test():
     data = cur.fetchall()
     cur.close()
     return str(data)
+
+@app.route('/uinterests')                       #its /uinterests?user_id=...  
+def return_userinterests():
+    user_id = request.args.get('user_id')       #need to change to request.form.get when real
+    if not user_id:
+       return "no user id"
+    
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM userInterests WHERE user_id = %s', (user_id,))
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
+
+@app.route('/set_uinterest', methods=['GET', 'POST'])     #its /set_uinterest?user_id=...&interest=....&scale=...
+def set_userinterest():
+    user_id = request.args.get('user_id')          #need to change to form
+    interest = request.args.get('interest')
+    scale = request.args.get('scale')
+    
+    if not user_id:
+       return "missing user id"
+    if not interest:
+       return "missing interest"
+    if not scale:
+       return "missing scale"
+    
+    cur = mysql.connection.cursor()
+    #original value (just for testing)
+    cur.execute('SELECT scale FROM userInterests WHERE user_id = %s AND interest = %s;', (user_id, interest))
+    old_scale = cur.fetchone()
+    #update
+    cur.execute('UPDATE userInterests SET scale = %s WHERE user_id = %s AND interest = %s;', (scale, user_id, interest))
+    mysql.connection.commit()  # commit changes
+    cur.close()
+  
+    return f"Interest changed for user {user_id}: {interest} {old_scale[0]} -> {scale}"
 
 if __name__ == '__main__':
     app.run(debug=True)
