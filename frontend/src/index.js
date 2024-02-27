a514105d02be4a1079533dd1e707cac555b1ea33
Oliver <Oliverpaynterjones@gmail.com>
2024-02-27 12:50:24 +0000

    changes to routing and global styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import ForYouPage from "./pages/ForYouPage/ForYouPage";
+import "./global-styles.css";
 
+function BodyStyleManager() {
+  const location = useLocation();
+  const currentPage = location.pathname.substring(1); // Remove the leading slash
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  // Set the body class dynamically
+  React.useEffect(() => {
+    document.body.className = currentPage + "-page-body";
+  }, [currentPage]);
+
+  return null;
 }
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/foryou/" element={<ForYouPage />} />
+    </Routes>
+    <BodyStyleManager />
+  </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
