3b2d872458c64d09726d40a8fef3cbbaaaf4436f
Oliver <Oliverpaynterjones@gmail.com>
2024-02-28 16:26:42 +0000

    polishing and fixes to styling of login page and global styles; updates to routing

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,40 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InputField from "../../Components/AnimtedInputField/InputField";
-
-// this is a fudge lol
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-document.body.style.alignContent = "center";
-document.body.style.minHeight = "100vh";
+import "./LoginPage.css"; // Import CSS file
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isCreateAccount, setIsCreateAccount] = useState(false);
+  const [containerHeight, setContainerHeight] = useState("21rem");
+  const [submitButtonWidth, setButtonWidth] = useState("6rem");
+
+  useEffect(() => {
+    setContainerHeight(isCreateAccount ? "25rem" : "21rem");
+  }, [isCreateAccount]);
+
+  useEffect(() => {
+    setButtonWidth(isCreateAccount ? "10rem" : "6rem"); // fix this
+  }, [isCreateAccount]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Form submitted with email:", email, "and password:", password);
+    if (isCreateAccount) {
+      console.log(
+        "Creating account with email:",
+        email,
+        "and password:",
+        password
+      );
+    } else {
+      console.log("Logging in with email:", email, "and password:", password);
+    }
+  };
+
+  const toggleMode = () => {
+    setIsCreateAccount(!isCreateAccount);
   };
 
   return (
-    <div
-      style={{
-        width: "680px",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h2
-        style={{
-          fontSize: "24px",
-          fontWeight: "400",
-          fontFamily: "'Google Sans','Noto Sans Myanmar UI',arial,sans-serif",
-          textAlign: "center",
-        }}
-      >
-        Sign in or register
-      </h2>
-      <form onSubmit={handleSubmit}>
+    <div className="login-container" style={{ height: containerHeight }}>
+      <h2 className="login-title">Sign in </h2>{" "}
+      <h2 className="login-subtitle"> or create your account</h2>
+      <form onSubmit={handleSubmit} className="login-form">
         <InputField
           type="email"
           fieldToAskFor="University Email"
@@ -47,12 +53,34 @@ const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button
-          type="submit"
-          style={{ width: "100%", boxSizing: "border-box" }}
-        >
-          Login
-        </button>
+        {isCreateAccount && (
+          <InputField
+            className="confirm-password-field"
+            type="password"
+            fieldToAskFor="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        <div className="button-container">
+          <button
+            type="button"
+            className="create-account-button"
+            onClick={toggleMode}
+          >
+            {isCreateAccount ? "Back to Login" : "Create New Account"}
+          </button>
+          <button
+            type="submit"
+            className="login-create-button"
+            style={{ width: submitButtonWidth }}
+          >
+            <span style={{ opacity: isCreateAccount ? 0 : 1 }}>Login</span>
+            <span style={{ opacity: isCreateAccount ? 1 : 0 }}>
+              Create Account
+            </span>
+          </button>
+        </div>
       </form>
     </div>
   );
