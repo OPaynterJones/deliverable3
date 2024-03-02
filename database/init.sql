f4bbe86ee1c02e18564dd53bb382c60b7d8477ee
AfonsoRM <124623524+AfonsoRM@users.noreply.github.com>
2024-03-02 16:56:55 +0000

    Update init.sql
    Added sessions table

--- database/init.sql
+++ database/init.sql
@@ -58,6 +58,12 @@ CREATE TABLE `userEvents` (
     FOREIGN KEY (`event_id`) REFERENCES `events`(`event_id`)
 );
 
+CREATE TABLE `sessions` (
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `session_token` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`user_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
+);
 
 -- Insert list of societies and some generic description
 INSERT INTO societies (name, description, picture) VALUES 
