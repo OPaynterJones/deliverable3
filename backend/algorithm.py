3c3e89e5f99191949b8b294203bae9776d814450
TomYMuirhead <tom.y.muirhead@gmail.com>
2024-03-12 17:29:37 +0000

    added society predictions

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -5,7 +5,7 @@ from sklearn.multioutput import MultiOutputRegressor
 import numpy as np
 
 def train():
-    data_file = './data.csv'
+    data_file = 'data.csv'
     try:
         #check if file is accessible
         print(f"data.csv file found: {os.path.isfile(data_file)}")
@@ -13,6 +13,7 @@ def train():
     except Exception as e:
         return (f"file not found, error: \n {e}")
         
+    
     interest_groups_col = data.iloc[:,-1]
     interest_list = ["ABACUS","Badminton","Basketball","Bath City FC soc","Boxing","Computer Science soc","Cricket","Cue sports","Cycling","Dance","Data science","Debate","Drum and Bass","Fashion","Finance","Fine art","Gin soc","Golf","Green Party","Hockey","Jiu Jitsu","Lacrosse","Left Union","Model UN","Music soc","Netball","Poker","Politics","Powerlifting","Rowing","Rugby union","Sailing","Salsa","Squash","Student Theatre","Swimming","Table Tennis","Triathlon","Urban Dance","Water polo"]
     # Remove the first column
@@ -22,8 +23,9 @@ def train():
     #X[X <= 5] = 1
     #X[X > 5] = 10
 
-    last_row = data.iloc[-1, 1:71]  # Exclude the first column (assuming it's not needed for prediction)
-    last_row = last_row.values.reshape(1, -1)
+    #last_row = data.iloc[-1, 1:71]  # Exclude the first column (assuming it's not needed for prediction)
+    #last_row = last_row.values.reshape(1, -1)
+
 
     cleaned_data = []
     for interest_groups_entry in interest_groups_col:
@@ -34,22 +36,39 @@ def train():
                 new_data[interest_list.index(interest)] = 1
         cleaned_data.append(new_data)
 
-    #print(cleaned_data)
+    print(X.iloc[0])
+
+
 
     base_regressor = RandomForestRegressor()
     multi_output_regressor = MultiOutputRegressor(base_regressor)
     multi_output_regressor.fit(X, cleaned_data)
 
-    last_row = data.iloc[-1, 1:71].values.reshape(1, -1)
-    last_row[last_row <= 5] = 3
-    last_row[last_row > 5] = 8
+    """
+    interests_vector = Take user interests from database
+    predicted_societies = multi_output_regressor.predict(interests_vector)
+    """
+    interest_vector = np.array([[7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4, 6, 3, 8, 1, 5, 10, 3, 7, 2, 9, 4]])
+    
+    np.reshape(interest_vector, (-1,1))
+    print(interest_vector.shape)
+    predicted_societies = multi_output_regressor.predict(interest_vector)
 
-    predicted_interests = multi_output_regressor.predict(last_row)
+    predictions = [(interest_list[i], val) for i, val in enumerate(predicted_societies[0])]
+    predictions.sort(key=lambda x: x[1], reverse=True)
+
+    print(predictions)
+
+    #last_row = data.iloc[-1, 1:71].values.reshape(1, -1)
+    #last_row[last_row <= 5] = 3
+    #last_row[last_row > 5] = 8
+
+    #predicted_interests = multi_output_regressor.predict(last_row)
 
     # Format the predictions
-    last_row_predictions = [(interest_list[i], val) for i, val in enumerate(predicted_interests[0])]
-    last_row_predictions.sort(key=lambda x: x[1], reverse=True)
+    #last_row_predictions = [(interest_list[i], val) for i, val in enumerate(predicted_interests[0])]
+    #last_row_predictions.sort(key=lambda x: x[1], reverse=True)
 
-    return(f"Predicted interest groups for the last row:\n {last_row_predictions}")
-
+    #print("Predicted interest groups for the last row:")
+    #print(last_row_predictions)
 print(train())
